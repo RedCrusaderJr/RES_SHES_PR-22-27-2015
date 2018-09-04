0846eaa75312d2d11b80be7e9924a29ba30f1c3c
unknown <dimitrijemitic1996@gmail.com>
2018-09-05 01:00:38 +0200

    Dodajem logiku klase UniversalTime

--- RES_SHES_PR-22-27-2015/Common/UniversalTime.cs
+++ RES_SHES_PR-22-27-2015/Common/UniversalTime.cs
@@ -9,22 +9,15 @@ namespace Common
 {
     public class UniversalTime
     {
+        #region Instance
         private static UniversalTime _instance;
-
-        private static object _timeLock = new object();
-
-        // datetime promenljiva
-
-        private UniversalTime()
-        {
-            
-        }
+        private static readonly object _timeLock = new object();
 
         public static UniversalTime S_UniversalTime
         {
             get
             {
-                if(_instance == null)
+                if (_instance == null)
                 {
                     lock (_timeLock)
                     {
@@ -34,7 +27,7 @@ namespace Common
                         }
                     }
                 }
-                
+
                 return _instance;
             }
 
@@ -43,5 +36,45 @@ namespace Common
                 _instance = value;
             }
         }
+        #endregion
+
+        private int _time;
+
+        public int Time
+        {
+            get
+            {
+                return (_time % (24 * 60 * 60)) + 1;
+            }
+
+            private set
+            {
+                _time = value;
+            }
+        }
+
+        public int GetTimeInMinutes()
+        {
+            return (Time / 60) + 1;
+        }
+
+        public double GetTimeInHours()
+        {
+            return (Time / (60 * 60)) + 1;
+        }
+
+        private UniversalTime()
+        {
+            Time = 0;
+            Task timeTracker = new Task( () => {
+                while (true)
+                {
+                    Time++;
+                    Thread.Sleep(1);
+                }
+            });
+
+            timeTracker.Start();
+        } 
     }
 }
--- RES_SHES_PR-22-27-2015/SHES/Program.cs
+++ RES_SHES_PR-22-27-2015/SHES/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SHES
@@ -11,6 +13,17 @@ namespace SHES
         static void Main(string[] args)
         {
             Console.WriteLine("SHES: Hello world!");
+
+            UniversalTime globalTime = UniversalTime.S_UniversalTime;
+
+            Console.WriteLine("Timer started");
+
+            while(true)
+            {
+                Console.WriteLine("Time: " + globalTime.Time + " seconds");
+                //Console.WriteLine("Time: " + globalTime.GetTimeInMinutes() + " minutes");
+                //Console.WriteLine("Time: " + globalTime.GetTimeInHours() + " hours");
+            }
         }
     }
 }
